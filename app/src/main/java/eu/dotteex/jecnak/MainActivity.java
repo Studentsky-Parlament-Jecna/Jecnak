e211960db94a071ce4462bb42ad533cd3caa221a
Ondřej Kulhavý <okulhav@gmail.com>
2022-07-17 01:10:53 +0200

    Minor update - jídelna fragment

--- app/src/main/java/eu/dotteex/jecnak/MainActivity.java
+++ app/src/main/java/eu/dotteex/jecnak/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             BottomNavigationView navView = findViewById(R.id.nav_view);
 
             AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.navigation_home, R.id.navigation_grades, R.id.navigation_profile)
+                    R.id.navigation_home, R.id.navigation_grades, R.id.navigation_canteen, R.id.navigation_profile)
                     .build();
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
