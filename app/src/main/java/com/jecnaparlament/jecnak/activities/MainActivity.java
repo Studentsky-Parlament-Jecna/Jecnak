694f3d69596a1b76e914c55370404ea798b58187
Ondřej Kulhavý <okulhav@gmail.com>
2022-08-29 08:36:49 +0200

    Fragment nastavení

--- app/src/main/java/com/jecnaparlament/jecnak/activities/MainActivity.java
+++ app/src/main/java/com/jecnaparlament/jecnak/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jecnaparlament.jecnak.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
@@ -35,9 +36,7 @@ public class MainActivity extends AppCompatActivity {
         }else {
             try {
                 controllers = new ControlerConection().execute().get();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (InterruptedException e) {
+            } catch (ExecutionException | InterruptedException e) {
                 e.printStackTrace();
             }
 
@@ -107,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, LoginActivity.class));
                 return true;
             case R.id.menu_about:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Dotteex/jecnak#readme")));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Studentsky-Parlament-Jecna/jecnak#readme")));
                 return true;
         }
         return false;
